39d3ff0ea3f6a43ea2f3092ec508b2a87d6a75fd
Mauricio Vargas <jonathan.vargas@gopenux.com>
2025-03-21 11:48:21 -0500

    Initial Commit

---
+++ NewsAppIOSTests/NewsAppIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppIOSTests.swift
+//  NewsAppIOSTests
+//
+//  Created by Radmas on 21/03/25.
+//
+
+import Testing
+@testable import NewsAppIOS
+
+struct NewsAppIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
